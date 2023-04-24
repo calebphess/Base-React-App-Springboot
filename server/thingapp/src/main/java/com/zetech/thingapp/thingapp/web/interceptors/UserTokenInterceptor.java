19cfd8899012c4742d7e0cebe811e10fbe4b3bb7
Caleb Hess <calebphess@gmail.com>
2023-04-24 03:59:18 -0400

    updated user token interceptor notes

--- server/thingapp/src/main/java/com/zetech/thingapp/thingapp/web/interceptors/UserTokenInterceptor.java
+++ server/thingapp/src/main/java/com/zetech/thingapp/thingapp/web/interceptors/UserTokenInterceptor.java
@@ -32,6 +32,29 @@ public class UserTokenInterceptor implements HandlerInterceptor
 
   //TODO: Source code I found this in had @Overrides statements above these methods
   // Figure out how required those are or why I don't seem to need them
+
+  /*
+   * Security Notes:
+   *  - Create a /token/get endpoint that generates a token when given a username and password
+   *    - Token has a timeout of 15 minutes as well as a userID
+   *  - Create a /token/refresh endpoint that takes a token and generates a new token IFF the last login date isn't older than 24 hours
+   *  - The user token interceptor should ALWAYS validate the token AND should ALWAYS validate the last login date
+   *    - This way someone can't re-generate tokens forever even if they get the token generation secret
+   *    - If I wanted to be crazy I could also roll the token generation secret every 24 hours and have it live in the database
+   *      - The downside to this is then it also has to be queried with the user every call
+   *      - I could also do something where the token generation secret is it's own token that expires every 24 hours that is saved in the user table with each user
+   *      - but then I would need a way to know which user was making the request so I'd have to add like a user-id to the header or something
+   *    - I should at least set up a schedule for rolling the oauth token secret
+   *    - There's a good article on making JWTs more secure here https://pragmaticwebsecurity.com/articles/apisecurity/hard-parts-of-jwt.html
+   *      - Notes from this article:
+   *        - It's a good idea to be able to invalidate users regardless of their token, probably by deactivating their account in the event of "suspicious activity" thorugh an "is_active" key
+   *          - Not a flag so you can store the key in the token and invalidate it when the key is null or is different
+   *        - Client side tokens should be stored as cookies
+   *        - TLS should be a requirement
+   *    - Another interesting article here https://www.jessym.com/articles/stateless-oauth2-social-logins-with-spring-boot#stateless-sessions-recurring
+   *  - Security is hard...
+  */
+
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception
   {
     Set<ApplicationRoles> roles = _securityService.authorize("test-user@thingapp.com", new SystemToken());
